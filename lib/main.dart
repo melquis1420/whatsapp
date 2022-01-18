1ae6afe1bd3b73fde575826288a0bf51895b115b
melquis1420 <46160910+melquis1420@users.noreply.github.com>
2022-01-17 21:40:09 -0300

    Login interface creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/Login.dart';
 import 'Home.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  Firestore.instance
-      .collection("usuarios")
-      .document("001")
-      .setData({"nome": "Melquis"});
-
   runApp(MaterialApp(
-    home: Home(),
+    home: Login(),
+    theme: ThemeData(
+      primaryColor: Color(0xff075E54),
+      accentColor: Color(0xff25D366),
+    ),
     debugShowCheckedModeBanner: false,
   ));
 }
