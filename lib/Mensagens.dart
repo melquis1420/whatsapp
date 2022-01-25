4eac09c3c6689d123afde0e8d64dcc33798b5a72
melquis1420 <46160910+melquis1420@users.noreply.github.com>
2022-01-25 20:36:12 -0300

    chat textBox configuration

--- lib/Mensagens.dart
+++ lib/Mensagens.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp/model/Usuario.dart';
 
@@ -11,13 +12,82 @@ class Mensagens extends StatefulWidget {
 }
 
 class _MensagensState extends State<Mensagens> {
+  TextEditingController _controllerMensagem = TextEditingController();
+
+  _enviarMensagem() {}
+
+  _enviarFoto() {}
+
   @override
   Widget build(BuildContext context) {
+    var caixaMensagem = Container(
+      padding: EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(right: 8),
+              child: TextField(
+                controller: _controllerMensagem,
+                //name controller
+                autofocus: true,
+                //set autofocus in name field
+                keyboardType: TextInputType.text,
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.fromLTRB(32, 8, 32, 8),
+                  hintText: "Mensagem",
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  prefixIcon: IconButton(
+                    icon: Icon(Icons.camera_alt),
+                    onPressed: _enviarFoto,
+                    color: Color(0xff075E54),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          FloatingActionButton(
+            backgroundColor: Color(0xff075E54),
+            child: Icon(
+              Icons.send,
+              color: Colors.white,
+            ),
+            mini: true,
+            onPressed: _enviarMensagem,
+          ),
+        ],
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.contato.nome),
+        backgroundColor: Color(0xff075E54),
       ),
-      body: Container(),
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/bg.png"), fit: BoxFit.cover),
+          ),
+          child: SafeArea(
+            child: Container(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  Text("listview"),
+                  caixaMensagem,
+                  //listview
+                  //caixa mensagem
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
